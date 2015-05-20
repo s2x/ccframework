fe150ce565edde10646e1b9f28dd72c33955c354
Piotr Hałas <piotr@halas.net.pl>
2015-05-20 21:48:31 +0200

    move to cmake

--- src/include/ccframework/ctemplate/config.h
+++ src/include/ccframework/ctemplate/config.h
@@ -1,165 +1,166 @@
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.ac by autoheader.  */
 
 /* always defined to indicate that i18n is enabled */
-#undef ENABLE_NLS
+#define ENABLE_NLS 1
 
 /* GETTEXT package name */
-#undef GETTEXT_PACKAGE
+#define GETTEXT_PACKAGE "libccframework"
 
 /* Namespace for Google classes */
-#undef GOOGLE_NAMESPACE
+#define GOOGLE_NAMESPACE ::ctemplate
 
 /* the location of <unordered_map> or <hash_map> */
-#undef HASH_MAP_H
+#define HASH_MAP_H <tr1/unordered_map>
 
 /* the namespace of hash_map/hash_set */
-#undef HASH_NAMESPACE
+#define HASH_NAMESPACE std::tr1
 
 /* the location of <unordered_set> or <hash_set> */
-#undef HASH_SET_H
+#define HASH_SET_H <tr1/unordered_set>
 
 /* Define to 1 if you have the `bind_textdomain_codeset' function. */
-#undef HAVE_BIND_TEXTDOMAIN_CODESET
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
 
 /* Define to 1 if you have the <boost/shared_ptr.hpp> header file. */
-#undef HAVE_BOOST_SHARED_PTR_HPP
+#define HAVE_BOOST_SHARED_PTR_HPP 1
 
 /* Define to 1 if you have the `dcgettext' function. */
-#undef HAVE_DCGETTEXT
+#define HAVE_DCGETTEXT 1
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-#undef HAVE_DLFCN_H
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the <fcgiapp.h> header file. */
-#undef HAVE_FCGIAPP_H
+#define HAVE_FCGIAPP_H 1
 
 /* Define if the GNU gettext() function is already present or preinstalled. */
-#undef HAVE_GETTEXT
+#define HAVE_GETTEXT 1
 
 /* define if the compiler has hash_map */
-#undef HAVE_HASH_MAP
+#define HAVE_HASH_MAP 1
 
 /* define if the compiler has hash_set */
-#undef HAVE_HASH_SET
+#define HAVE_HASH_SET 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define if your <locale.h> file defines LC_MESSAGES. */
-#undef HAVE_LC_MESSAGES
+#define HAVE_LC_MESSAGES 1
 
 /* Define to 1 if you have the <locale.h> header file. */
-#undef HAVE_LOCALE_H
+#define HAVE_LOCALE_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* define if the compiler implements namespaces */
-#undef HAVE_NAMESPACES
+#define HAVE_NAMESPACES 1
 
 /* Define if you have POSIX threads libraries and header files. */
-#undef HAVE_PTHREAD
+#define HAVE_PTHREAD 1
 
 /* define if the compiler implements pthread_rwlock_* */
-#undef HAVE_RWLOCK
+#define HAVE_RWLOCK 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if the system has the type `uint32_t'. */
-#undef HAVE_UINT32_T
+#define HAVE_UINT32_T 1
 
 /* Define to 1 if the system has the type `uint64_t'. */
-#undef HAVE_UINT64_T
+#define HAVE_UINT64_T 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* define if the compiler supports unordered_{map,set} */
-#undef HAVE_UNORDERED_MAP
+#define HAVE_UNORDERED_MAP 1
 
 /* Define to 1 if the system has the type `u_int32_t'. */
-#undef HAVE_U_INT32_T
+#define HAVE_U_INT32_T 1
 
 /* Define to 1 if the system has the type `u_int64_t'. */
-#undef HAVE_U_INT64_T
+#define HAVE_U_INT64_T 1
 
 /* Define to 1 if the system has the type `__int32'. */
-#undef HAVE___INT32
+/* #undef HAVE___INT32 */
 
 /* Define to 1 if the system has the type `__int64'. */
-#undef HAVE___INT64
+/* #undef HAVE___INT64 */
 
 /* The namespace to put the htmlparser code. */
-#undef HTMLPARSER_NAMESPACE
+#define HTMLPARSER_NAMESPACE google_ctemplate_streamhtmlparser
 
 /* define if first argument to InterlockedExchange is just LONG */
-#undef INTERLOCKED_EXCHANGE_NONVOLATILE
+#define INTERLOCKED_EXCHANGE_NONVOLATILE 1
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
-#undef LT_OBJDIR
+#define LT_OBJDIR ".libs/"
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "libccframework"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT ""
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "libccframework"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "libccframework 0.1"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "libccframework"
 
 /* Define to the home page for this package. */
-#undef PACKAGE_URL
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "0.1"
 
 /* printf format code for printing a size_t and ssize_t */
-#undef PRIdS
+#define PRIdS "ld"
 
 /* printf format code for printing a size_t and ssize_t */
-#undef PRIuS
+#define PRIuS "lu"
 
 /* printf format code for printing a size_t and ssize_t */
-#undef PRIxS
+#define PRIxS "lx"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
-#undef PTHREAD_CREATE_JOINABLE
+/* #undef PTHREAD_CREATE_JOINABLE */
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* the namespace where STL code like vector<> is defined */
-#undef STL_NAMESPACE
+#define STL_NAMESPACE std
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "0.1"
 
 /* Stops putting the code inside the Google namespace */
-#undef _END_GOOGLE_NAMESPACE_
+#define _END_GOOGLE_NAMESPACE_ }
 
 /* Puts following code inside the Google namespace */
-#undef _START_GOOGLE_NAMESPACE_
+#define _START_GOOGLE_NAMESPACE_ namespace ctemplate {
