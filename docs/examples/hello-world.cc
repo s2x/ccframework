a44ce3bd8c92ce199fce6bf89db337a5af3c9db2
Piotr Hałas <piotr@halas.net.pl>
2015-05-21 11:04:19 +0200

    add docs and pkgconfig file

---
+++ docs/examples/hello-world.cc
@@ -0,0 +1,53 @@
+﻿/*
+ ============================================================================
+ Name        : hello-world.cc
+ Author      : Piotr Hałas
+ Version     :
+ Copyright   : 
+ Description : Hello World in ccFramework,
+ Compile     : g++ hello-world.cc -std=c++11 `pkg-config --cflags --libs ccframework` -lfcgi++ -lfcgi  -o hello-world.fcgi
+ ============================================================================
+ */
+
+#include "ccFramework.h"
+
+using namespace std;
+using namespace ccFramework;
+
+
+// controlller class 
+class ExampleController: public ccController {
+public:
+    //custom constructor is needed
+    ExampleController(ccApp *app): ccController(app) {};
+    
+    //very simple controller, just print hello world
+    ccResponse *indexAction(ccRequest *request) {
+        //creates new response with Hello world message
+        return (new ccResponse("Hello world!!!"));
+    }
+    
+    // registerRoutes is called by ccApp 
+    void registerRoutes() {
+        //register route
+        this->getRouter()->addRoute(
+                // create new static route
+                new ccStaticRoute("index", "/", 
+                
+                    //macro for easy creating new functor
+                    create_routing_functor(ExampleController,indexAction)));
+    }
+};
+
+int main(void) {
+    //initailise main app, set the config file
+    ccApp app("../config.yml");
+
+    //register example controller 
+    app.registerController("example",new ExampleController(&app));
+    
+    //main loop. Wait for connection
+    app.run();
+
+    return 0;
+}
